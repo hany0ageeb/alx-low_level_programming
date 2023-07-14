f2c8ed9fe18be853eb4e9c76a0e14913d25d5182
hany0ageeb <hany0ageeb@gmail.com>
2023-07-14 01:58:04 -0700

    adding file 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,16 +5,16 @@
  */
 int main(void)
 {
-  char c = '0';
-  do {
-    putchar(c);
-    if (c >= '0' && c < '9')
-      c++;
-    else if (c == '9')
-      c = 'a';
-    else if (c >= 'a')
-      c++;
-  } while (c <= 'f');
-  putchar('\n');
-  return (0);
+char c = '0';
+do {
+putchar(c);
+if (c >= '0' && c < '9')
+c++;
+else if (c == '9')
+c = 'a';
+else if (c >= 'a')
+c++;
+} while (c <= 'f');
+putchar('\n');
+return (0);
 }
