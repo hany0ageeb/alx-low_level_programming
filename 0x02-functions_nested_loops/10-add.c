ac17e7c0bcb33c15f04a601f9bd3ec8207108202
hany0ageeb <hany0ageeb@gmail.com>
2023-07-18 06:08:06 -0700

    adding file 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+ * add - a function that adds two integers and returns the result.
+ * @a: the first integer to add
+ * @b: the second integer to add
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
