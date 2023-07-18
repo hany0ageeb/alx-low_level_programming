cb9d3afc499cf01b35c8e09a005db28caaa5a8fc
hany0ageeb <hany0ageeb@gmail.com>
2023-07-18 11:28:37 -0700

    adding 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * print_fibonacci - print first n fibonacci num
+ * @n: number of fibonacci series
+ */
+void print_fibonacci(int n)
+{
+	long int f0 = 0, f1 = 1, f;
+
+	if (n < 0)
+		return;
+	if (n >= 1)
+	{
+		printf("%ld", f0);
+	}
+	if (n >= 2)
+	{
+		printf(", %ld", f1);
+	}
+	while (n >= 3)
+	{
+		f = f1 + f0;
+		printf(", %ld", f);
+		f0 = f1;
+		f1 = f;
+		n--;
+	}
+	printf("\n");
+}
+/**
+ * main - Entry point
+ * Return: always 0
+*/
+int main(void)
+{
+	print_fibonacci(50);
+	return (0);
+}
+
