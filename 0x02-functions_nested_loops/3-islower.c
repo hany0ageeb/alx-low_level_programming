118308cc8ff8bf98a4f3af426930f7d74a144202
hany0ageeb <hany0ageeb@gmail.com>
2023-07-18 05:05:56 -0700

    adding file 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+/**
+ * _islower - check if a letter is small
+ * @c: the letter to check if small
+ * Return: 1 if small letter otherwise 0
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122 ? 1 : 0);
+}
+
