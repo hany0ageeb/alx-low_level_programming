5d82c574403289ff8795107d251583da9cb1abd7
hany0ageeb <hany0ageeb@gmail.com>
2023-07-19 05:09:42 -0700

    removing inline function is_leap_year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,17 +2,6 @@
 #include "main.h"
 
 /**
- * is_leap_year - takes a year and determines if it is a leap year or not
- * @year: year
- * Return: 1 if year is leap otherwise 0
- */
-inline int is_leap_year(int year)
-{
-	if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
-		return (1);
-	return (0);
-}
-/**
 * print_remaining_days - takes a date and prints how many days are
 * left in the year, taking leap years into account
 * @month: month in number format
@@ -23,7 +12,7 @@ inline int is_leap_year(int year)
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (is_leap_year(year) == 1)
+	if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
