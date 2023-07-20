0ae707c964c0b4218ad7e38bfe83f29594a09e75
hany0ageeb <hany0ageeb@gmail.com>
2023-07-20 01:22:26 -0700

    adding file 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+/**
+ * _isdigit - a function that checks for a digit (0 through 9).
+ * @c: charachter
+ * Return: 1 if c is 0 - 9 otherwise 0.
+ */
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57 ? 1 : 0);
+}
+
