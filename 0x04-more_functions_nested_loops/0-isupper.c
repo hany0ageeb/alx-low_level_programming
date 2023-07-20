f47147075800545168f46bf95630644a205c8a4e
hany0ageeb <hany0ageeb@gmail.com>
2023-07-20 01:18:47 -0700

    adding file 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,11 @@
+/**
+ * _isupper - a function that checks for uppercase character.
+ * @c: character
+ * Return: 1 if c is uppercase 0 otherwise
+ */
+
 int _isupper(int c)
 {
-	return c >= 65 && c <= 90 ? 1 : 0;
+	return (c >= 65 && c <= 90 ? 1 : 0);
 }
 
