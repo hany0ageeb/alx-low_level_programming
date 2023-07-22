fcec95a065ecb03bc3f2dbed6bef8cb44a222354
hany0ageeb <hany0ageeb@gmail.com>
2023-07-22 07:04:21 -0700

    adding 101-print_number.c last attempt!

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 
 /**
@@ -11,7 +12,6 @@ int count_number_digits(int num)
 {
 	int count = 0;
 
-	/*num = num < 0 ? num * -1 : num;*/
 	while (num != 0)
 	{
 		num /= 10;
@@ -22,6 +22,34 @@ int count_number_digits(int num)
 	return (count);
 }
 /**
+ * print_last_digit - a function to print last digit int num
+ * @num: the number (integer)
+ * Return: the remainig of the number after removing the printed digit.
+ */
+int print_last_digit(int num)
+{
+	int base = 1, base1 = 1, remainder = 0, temp_num = num;
+
+	while (num != 0)
+	{
+		remainder = num % 10;
+		num /= 10;
+		if (base < 1000000000)
+			base *= 10;
+		else
+			base *= 10;
+	}
+	if (base1 == 1)
+		num = temp_num - remainder * (base / 10);
+	else
+		num = temp_num - remainder * base;
+	if (remainder >= 0)
+		_putchar(48 + remainder);
+	else
+		_putchar (48 + remainder * -1);
+	return (num);
+}
+/**
  * print_number - a function that prints an integer.
  * @n: an integer to print
  * Return: void
@@ -30,40 +58,17 @@ int count_number_digits(int num)
 void print_number(int n)
 {
 	int digits_count = count_number_digits(n);
-	int n1, remainder, base, printed_digits = 0, remaining_digit_count, base1;
+	int printed_digits = 0, remaining_digit_count;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		/*n *= -1;*/
 	}
-	n1 = n;
 	while (printed_digits < digits_count)
 	{
-		base = 1;
-		base1 = 1;
-		remainder = 0;
-		while (n1 != 0)
-		{
-			remainder = n1 % 10;
-			n1 /= 10;
-			if (base < 1000000000)
-				base *= 10;
-			else
-				base1 *= 10;
-			/*printf("%d\n", base);*/
-		}
-		if (base1 == 1)
-			n1 = n - remainder * (base / 10);
-		else
-			n1 = n - remainder * base;
-		n = n1;
-		if (remainder >= 0)
-			_putchar(48 + remainder);
-		else
-			_putchar(48 + remainder * -1);
+		n = print_last_digit(n);
 		printed_digits++;
-		remaining_digit_count = count_number_digits(n1);
+		remaining_digit_count = count_number_digits(n);
 		while (remaining_digit_count < (digits_count - printed_digits))
 		{
 			_putchar('0');
