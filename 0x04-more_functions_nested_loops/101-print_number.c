9ee4eac3da946984f5c3d531c0dffdc94e519dc8
hany0ageeb <hany0ageeb@gmail.com>
2023-07-22 05:34:43 -0700

    adding 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * count_number_digits - count the number of digits in a given integer.
  * for example if num =  24 will return 2 if num = 500 will return 3
@@ -29,7 +30,7 @@ int count_number_digits(int num)
 void print_number(int n)
 {
 	int digits_count = count_number_digits(n);
-	int n1, remainder, base, printed_digits = 0, remaining_digit_count;
+	int n1, remainder, base, printed_digits = 0, remaining_digit_count, base1;
 
 	if (n < 0)
 	{
@@ -40,14 +41,22 @@ void print_number(int n)
 	while (printed_digits < digits_count)
 	{
 		base = 1;
+		base1 = 1;
 		remainder = 0;
 		while (n1 != 0)
 		{
 			remainder = n1 % 10;
 			n1 /= 10;
-			base *= 10;
+			if (base < 1000000000)
+				base *= 10;
+			else
+				base1 *= 10;
+			/*printf("%d\n", base);*/
 		}
-		n1 = n - remainder * (base / 10);
+		if (base1 == 1)
+			n1 = n - remainder * (base / 10);
+		else
+			n1 = n - remainder * base;
 		n = n1;
 		if (remainder >= 0)
 			_putchar(48 + remainder);
