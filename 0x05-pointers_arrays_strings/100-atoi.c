adbbf4aa548b3277906e84c573182aa307fe9549
hany0ageeb <hany0ageeb@gmail.com>
2023-07-24 04:44:46 -0700

    adding file 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,21 +7,35 @@
 
 int _atoi(char *s)
 {
-	int last_index = 0, base = 1, result = 0;
+	int last_index = 0, base = 1, result = 0, first_index = 0;
 
-	while (s[last_index] != '\0')
+	while ((s[first_index] < '0' || s[first_index] > '9') && s[first_index] != '\0')
+		first_index++;
+	if (s[first_index] == '\0')
+	{
+		first_index--;
+		if (first_index == -1)
+			return (0);
+	}
+	else if (s[first_index] < '0' || s[first_index] > '9')
+	{
+		return (0);
+	}
+	last_index = first_index;
+	while (s[last_index] >= '0' && s[last_index] <= '9' && s[last_index] != '\0')
 		last_index++;
 	last_index--;
-	while (last_index >= 0 && s[last_index] >= '0' && s[last_index] <= '9')
+	while (last_index >= first_index && s[last_index] >= '0' && s[last_index] <= '9')
 	{
 		result += (s[last_index] - 48) * base;
 		base *= 10;
 		last_index--;
 	}
-	if (last_index >= 0 && s[last_index] == '-')
-	{
-		result *= -1;
-	}
-	return (result);
+	while (last_index >= 0 && s[last_index] != '-' && (s[last_index] == ' ' || s[last_index]=='+' || s[last_index]=='\t'))
+		last_index--;
+	if (last_index < 0)
+		return (result);
+	else
+		return (result * -1);
 }
 
