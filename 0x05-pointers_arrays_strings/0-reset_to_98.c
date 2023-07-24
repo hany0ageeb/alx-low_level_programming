21071b6c550f9c04a894d8041af7f3440c04b0b4
hany0ageeb <hany0ageeb@gmail.com>
2023-07-24 00:26:02 -0700

    adding readme.md file, main.h, 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n: a pointer to an integer
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+trmp read me file
