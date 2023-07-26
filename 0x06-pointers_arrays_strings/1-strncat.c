61a6094b9adaaa260e3c403a4e0ed1a7146fce28
hany0ageeb <hany0ageeb@gmail.com>
2023-07-26 10:54:42 -0700

    fix 1-strncat.c formatting to satisfy betty

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,14 +2,15 @@
  * _strncat - con
  * @dest: destinqtion string
  * @src: source string
- * Returns: dest+src
+ * @n: max number of char to copy from souce to destination
+ * Return: dest+src
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (i=0; dest[i] != '\0'; ++i)
-			;
+	for (i = 0; dest[i] != '\0'; ++i)
+		;
 	for (j = 0; j < n && src[j] != '\0'; ++j, ++i)
 		dest[i] = src[j];
 	return (dest);
