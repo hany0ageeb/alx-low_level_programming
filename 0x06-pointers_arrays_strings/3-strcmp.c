e1d3d9cc29fab67544b5f1220011ca1afc4e4662
hany0ageeb <hany0ageeb@gmail.com>
2023-07-26 11:35:24 -0700

    fixing betty issues with 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,7 +2,8 @@
  * _strcmp - a function that compares two strings.
  * @s1: first string
  * @s2: second string
- * Return: 
+ * Return: 0 if s1 equals s2 a positive value if s1 > s2
+ * a negative value if s1 < s2
  */
 
 int _strcmp(char *s1, char *s2)
@@ -14,11 +15,11 @@ int _strcmp(char *s1, char *s2)
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
 	}
-	if(s1[i] != '\0' || s2[i] != '\0')
+	if (s1[i] != '\0' || s2[i] != '\0')
 	{
 		return (s1[i] - s2[i]);
 	}
-	return 0;
+	return (0);
 
 }
 
