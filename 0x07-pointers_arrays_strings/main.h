b8d6afefeba56c25ad8cca34bf225a5162dc10b4
hany0ageeb <hany0ageeb@gmail.com>
2023-08-01 02:52:26 -0700

    adding main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,6 +9,8 @@ void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 void print_diagsums(int *a, int size);
 unsigned int _strspn(char *s, char *accept);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif
 
