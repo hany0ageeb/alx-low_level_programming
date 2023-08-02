99b496e78b1992d549c21c36012472212aee3a15
hany0ageeb <hany0ageeb@gmail.com>
2023-08-02 03:45:37 -0700

    fixing issues with betty and file 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,7 +1,7 @@
 /**
  * factorial - a function that returns the factorial of a given number.
  * @n: integer
- * Return: -1 if n < 0 1 if n  equals 0 or 1 
+ * Return: -1 if n < 0 1 if n  equals 0 or 1
  */
 int factorial(int n)
 {
