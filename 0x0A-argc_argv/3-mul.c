bf591fb85f7110d2a0a39ca5fa6d04f2676eee25
hany0ageeb <hany0ageeb@gmail.com>
2023-08-04 04:01:40 -0700

    adding 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+/**
+ * is_number - check s is valid int
+ * @s: string
+ * Return: 1 if valid 0 otherwise.
+ */
+int is_number(char *s)
+{
+	if (s == NULL)
+		return (0);
+	if (*s == '\0')
+		return (0);
+	if (*s == '-' || *s == '+')
+		s++;
+	while (*s != '\0')
+	{
+		if (*s < '0' || *s > '9')
+			return (0);
+		s++;
+	}
+	return (1);
+}
 /**
  * main - a program that multiplies two numbers.
  * @argc: args count
@@ -11,6 +33,11 @@ int main(int argc, char *argv[])
 	int a, b;
 
 	if (argc != 3)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+	if (!is_number(argv[1]) || !is_number(argv[1]))
 	{
 		printf("ERROR\n");
 		return (1);
