53077c3730207a5a680f9dfa33d34441e1b76296
hany0ageeb <hany0ageeb@gmail.com>
2023-08-10 00:23:08 -0700

    adding file 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,10 +11,29 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr = NULL;
+	char *temp = NULL;
+	unsigned int i, initialized_size;
 
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 	ptr = malloc(nmemb * size);
+	if (ptr != NULL)
+	{
+		temp = (char *)ptr;
+		i = 0;
+		initialized_size = 0;
+		while (i < nmemb)
+		{
+			*temp = 0;
+			temp++;
+			initialized_size += (unsigned int)sizeof(char);
+			if (initialized_size == size)
+			{
+				i++;
+				initialized_size = 0;
+			}
+		}
+	}
 	return (ptr);
 }
 
