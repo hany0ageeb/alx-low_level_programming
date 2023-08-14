da113b0858ada41a9ccadb37bc6f1d0fd90c5f42
hany0ageeb <hany0ageeb@gmail.com>
2023-08-14 04:23:46 -0700

    adding file 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,48 @@
+#include "dog.h"
+#include "stdlib.h"
+/**
+ * _strcpy - make a copy of src string
+ * @src: source string to copy
+ * Return: returned a pointer  to newly created string copy.
+ */
+char *_strcpy(char *src)
+{
+	char *str = 0;
+	int i = 0;
+
+	if (src != 0)
+	{
+		while (src[i] != '\0')
+			i++;
+		str = malloc(sizeof(char) * i + 1);
+		i = 0;
+		while (src[i] != '\0')
+		{
+			str[i] = src[i];
+			i++;
+		}
+		str[i] = '\0';
+
+	}
+	return (str);
+}
+/**
+ * new_dog - a function that creates a new dog.
+ * @name: new dog name.
+ * @age: dog age
+ * @owner: dog owner
+ * Return: a pointer to the newly created dog or null if failed
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d = malloc(sizeof(dog_t));
+
+	if (d != 0)
+	{
+		d->name = _strcpy(name);
+		d->age = age;
+		d->owner = _strcpy(owner);
+	}
+	return (d);
+}
+
