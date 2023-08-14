eb89b15f280362c76ac698ea153d2582f4d36532
hany0ageeb <hany0ageeb@gmail.com>
2023-08-14 05:50:18 -0700

    adding file 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,8 +7,13 @@
  */
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d != (void *)0)
+	{
+		if (d->name != (void *)0)
+			free(d->name);
+		if (d->owner != (void *)0)
+			free(d->owner);
+		free(d);
+	}
 }
 
