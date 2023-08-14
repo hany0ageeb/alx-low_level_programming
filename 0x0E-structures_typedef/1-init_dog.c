a289b870f71e73b781aa01e99692b8ec01a62a0f
hany0ageeb <hany0ageeb@gmail.com>
2023-08-14 05:29:34 -0700

    adding file 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,33 @@
 #include "dog.h"
+#include <stdlib.h>
+/**
+ * _strcpy - copy a string.
+ * @src: source string
+ * Return: apointer to the copy of src.
+ */
+char *_strcpy(char *src)
+{
+	char *str = (void *)0;
+	int i = 0;
+
+	if (src != (void *)0)
+	{
+		while (src[i] != '\0')
+			i++;
+		str = malloc(sizeof(char) * i + 1);
+		if (str != (void *)0)
+		{
+			i = 0;
+			while (src[i] != '\0')
+			{
+				str[i] = src[i];
+				i++;
+			}
+			str[i] = '\0';
+		}
+	}
+	return (str);
+}
 /**
  * init_dog - a function that initialize a variable of type struct dog
  * @d: a dog pointer to initialize.
@@ -10,8 +39,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
+	d->name = _strcpy(name);
 	d->age = age;
-	d->owner = owner;
+	d->owner = _strcpy(owner);
 }
 
