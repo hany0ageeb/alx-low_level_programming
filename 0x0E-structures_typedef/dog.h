a30a57b1164e6b27d106c5915cf1ef5faf5c0396
hany0ageeb <hany0ageeb@gmail.com>
2023-08-14 03:41:51 -0700

    adding file dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - define a new type dog.
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+
