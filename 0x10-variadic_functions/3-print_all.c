e9421b8e634eba13dd077d385d3383279baafe45
hany0ageeb <hany0ageeb@gmail.com>
2023-08-17 05:30:04 -0700

    adding file 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,34 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX_FORMAT_SIZE 100
+/**
+ * filter_format - filter format to only contain c, i, f, s
+ * @format: original format
+ * Return: apointer to filterd format
+ */
+char *filter_format(const char * const format)
+{
+	char *fmt = malloc(sizeof(char) * MAX_FORMAT_SIZE + 1);
+	unsigned int i = 0, j = 0;
+
+	while (format != (void *)0 && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+			case 'i':
+			case 'f':
+			case 's':
+				fmt[j] = format[i];
+				j++;
+				break;
+		}
+		i++;
+	}
+	fmt[j] = '\0';
+	return (fmt);
+}
 /**
  * print_all - a function that prints anything.
  * @format: is a list of types of arguments passed to the function
@@ -8,13 +37,13 @@ void print_all(const char * const format, ...)
 {
 	unsigned int i = 0;
 	va_list argptr;
-	char *nil_str = "(nil)";
-	char *str = (void *)0;
+	char *nil_str = "(nil)", *str = (void *)0, *fmt;
 
 	va_start(argptr, format);
-	while (format != (void *)0 && format[i] != '\0')
+	fmt = filter_format(format);
+	while (fmt != (void *)0 && fmt[i] != '\0')
 	{
-		switch (format[i])
+		switch (fmt[i])
 		{
 			case 'c':
 				printf("%c", va_arg(argptr, int));
@@ -33,9 +62,12 @@ void print_all(const char * const format, ...)
 				}
 				printf("%s", str);
 		}
+		if (fmt[i + 1] != '\0')
+			printf(", ");
 		i++;
 	}
 	va_end(argptr);
+	free(fmt);
 	printf("\n");
 }
 
