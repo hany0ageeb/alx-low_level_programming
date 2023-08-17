8a2992176b985d86e97349942adaa600321bfa39
hany0ageeb <hany0ageeb@gmail.com>
2023-08-17 04:12:48 -0700

    adding 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	int arg;
 
 	if (n <= 0)
+	{
+		printf("\n");
 		return;
+	}
 	va_start(argptr, n);
 	for (i = 0; i < n; ++i)
 	{
