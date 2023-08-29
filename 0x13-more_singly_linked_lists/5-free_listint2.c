d60a66bbe84d0f83544e9853c29623791fcdb3f7
hany0ageeb <hany0ageeb@gmail.com>
2023-08-29 11:13:57 -0700

    5-free_listint2.c with loop detection

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,6 +1,26 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
+ * detect_loop - detects loop
+ * @head: head
+ * Return: null ot nodeadd
+ */
+listint_t *detect_loop(listint_t *head)
+{
+	listint_t *ps, *pf;
+
+	ps = head;
+	pf = head;
+	while (ps != NULL && pf != NULL && pf->next != NULL)
+	{
+		ps = ps->next;
+		pf = pf->next->next;
+		if (ps == pf)
+			return (ps);
+	}
+	return (NULL);
+}
+/**
  * free_listint2 - function that frees a listint_t list.
  * @head: head node
  */
@@ -8,6 +28,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *end, *pre_end;
 
+	end = detect_loop(*head);
+	if (end != NULL)
+	{
+		if (end == *head)
+		{
+			*head = end->next;
+		}
+		end->next = NULL;
+	}
 	while (*head != NULL && (*head)->next != NULL)
 	{
 		pre_end = *head;
