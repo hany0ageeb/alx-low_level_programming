6e78ac78ba45f7a4b16b8045408dfcc842356e19
hany0ageeb <hany0ageeb@gmail.com>
2023-09-04 04:30:37 -0700

    adding file 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,7 +2,6 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
 /**
  * _cp - copy from fd_from to fd_to
  * @fd_from: from fd
@@ -70,7 +69,6 @@ int main(int argc, char **argv)
 			exit(100);
 		}
 	}
-	chmod(argv[2], 0664);
 	exit(EXIT_SUCCESS);
 }
 
