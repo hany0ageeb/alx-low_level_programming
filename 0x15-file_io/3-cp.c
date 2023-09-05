a156d56fffe1393df259f5401bfb5049f6c21e22
hany0ageeb <hany0ageeb@gmail.com>
2023-09-05 04:03:30 -0700

    adding file 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,16 +6,28 @@
  * _cp - copy from fd_from to fd_to
  * @fd_from: from fd
  * @fd_to: to fd
+ * @from_file_name: from file name
  * @to_file_name: to file name
  * Return: void
  */
-void _cp(int fd_from, int fd_to, char *to_file_name)
+void _cp(int fd_from, int fd_to, const char *to_file_name,
+		const char *from_file_name)
 {
-	size_t r = 0, w = 0;
+	ssize_t r = 0, w = 0;
 	char buffer[1025];
 
-	while ((r = read(fd_from, buffer, 1024)) > 0)
+	while (1)
 	{
+		r = read(fd_from, buffer, 1024);
+		if (r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from_file_name);
+			close(fd_from);
+			close(fd_to);
+			exit(98);
+		}
+		else if (r == 0)
+			break;
 		buffer[r] = '\0';
 		w = write(fd_to, buffer, r);
 		if (r != w)
@@ -48,13 +60,13 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	fd_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0666);
 	if (fd_to < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	_cp(fd_from, fd_to, argv[2]);
+	_cp(fd_from, fd_to, argv[2], argv[1]);
 	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
