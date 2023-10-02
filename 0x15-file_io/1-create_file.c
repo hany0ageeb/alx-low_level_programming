73c00a66a5203f4f0c205a28f8b4fcd555367bdc
hany0ageeb <hany0ageeb@gmail.com>
2023-10-02 04:00:40 -0700

    fixing issue in file 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 	size_t len;
 	ssize_t n_write;
 
-	fout = open(filename, O_WRONLY | O_CREAT, 0600);
+	fout = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fout != -1)
 	{
 		if (text_content != NULL)
